e93f1cc122be687106f8b44ee2ead299c43dcdf6
bsameer <bsameer@DTPGCC616>
2018-05-18 12:37:28 +0530

    Add project files.

---
+++ WebResourceUpdate/UpdateResource.js
@@ -0,0 +1,7 @@
+﻿function Set() {
+    Xrm.Page.ui.controls.get("telephone1").setDisabled(true);
+    var telephonenum = Xrm.Page.getAttribute("telephone1").getValue();
+    alert(telephonenum);
+    Xrm.Page.getAttribute("mobilephone").setValue(telephonenum);
+    Xrm.Page.getAttribute("emailaddress1").setValue("bsameer1995@gmail.com");
+}
